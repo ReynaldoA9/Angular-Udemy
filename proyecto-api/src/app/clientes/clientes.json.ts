340d1c87f8932223a3c87a63727335740d8589b0
ReynaldoA9 <reynaldo.silva.external@atos.net>
2023-04-18 17:08:20 +0100

    Procedemos a crear rutas en Angular
    Vamos a configurar las rutas como ya hicimos en springREST en Eclipse

---
+++ proyecto-api/src/app/clientes/clientes.json.ts
@@ -0,0 +1,20 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Reynaldo', apellido: 'Duque', email: 'reynaldo.atos@gmail.com', createAt: '2023-04-18'},
+  {id: 2, nombre: 'María', apellido: 'García', email: 'maria.garcia@gmail.com', createAt: '2023-02-17'},
+  {id: 3, nombre: 'Juan', apellido: 'Pérez', email: 'juan.perez@gmail.com', createAt: '2023-05-16'},
+  {id: 4, nombre: 'Ana', apellido: 'Martínez', email: 'ana.martinez@gmail.com', createAt: '2023-08-01'},
+  {id: 5, nombre: 'Pedro', apellido: 'González', email: 'pedro.gonzalez@gmail.com', createAt: '2023-01-18'},
+  {id: 6, nombre: 'Lucía', apellido: 'Fernández', email: 'lucia.fernandez@gmail.com', createAt: '2023-04-20'},
+  {id: 7, nombre: 'Carlos', apellido: 'Sánchez', email: 'carlos.sanchez@gmail.com', createAt: '2023-04-05'},
+  {id: 8, nombre: 'Laura', apellido: 'López', email: 'laura.lopez@gmail.com', createAt: '2023-07-12'},
+  {id: 9, nombre: 'Javier', apellido: 'Hernández', email: 'javier.hernandez@gmail.com', createAt: '2023-09-11'},
+  {id: 10, nombre: 'Sofía', apellido: 'Díaz', email: 'sofia.diaz@gmail.com', createAt: '2023-04-15'},
+  {id: 11, nombre: 'Diego', apellido: 'Ruiz', email: 'diego.ruiz@gmail.com', createAt: '2023-08-19'},
+  {id: 12, nombre: 'Marcela', apellido: 'Gómez', email: 'marcela.gomez@gmail.com', createAt: '2023-03-29'},
+  {id: 13, nombre: 'Luis', apellido: 'Torres', email: 'luis.torres@gmail.com', createAt: '2023-03-18'},
+  {id:  14, nombre: 'Fernanda', apellido: 'Ramírez', email: 'fernanda.ramirez@gmail.com', createAt: '2023-03-28'},
+  {id: 15, nombre: 'Ricardo', apellido: 'Vargas', email: 'ricardo.vargas@gmail.com', createAt: '2023-03-08'},
+  {id: 16, nombre: 'Mariana', apellido: 'Castillo', email: 'mariana.castillo@gmail.com', createAt: '2023-01-10'}
+];
