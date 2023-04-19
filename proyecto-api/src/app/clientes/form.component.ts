b087430396147cb2afbf4418017199ba3865ede4
ReynaldoA9 <reynaldo.silva.external@atos.net>
2023-04-19 23:18:25 +0100

    Final del front-end
    Terminamos la carpeta 10 y el único error que tengo es que el boton editar no me llega a funcionar (extraño php)

--- proyecto-api/src/app/clientes/form.component.ts
+++ proyecto-api/src/app/clientes/form.component.ts
@@ -1,8 +1,8 @@
-import { ClienteService } from './cliente.service';
 import { Component } from '@angular/core';
 import { Cliente } from './cliente';
-import { ActivatedRoute, Router } from '@angular/router';
-import swal from 'sweetalert2'
+import { ClienteService } from './cliente.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -12,6 +12,7 @@ export class FormComponent {
 
   public cliente: Cliente = new Cliente();
   public titulo:string = "Crear Cliente";
+  public errores: string[] = [""];
 
   constructor(private clienteService: ClienteService,
     private router: Router,
@@ -21,11 +22,14 @@ export class FormComponent {
     this.cargarCliente();
   }
 
-  cargarCliente(): void {
+  cargarCliente():void{
     this.activatedRoute.params.subscribe(params => {
       let id = params['id']
       if(id) {
-        this.clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente)
+        this.clienteService.getCliente(id).subscribe((cliente) =>{
+          this.cliente = cliente
+          this.cliente.createAt = this.cliente.createAt.split("T")[0];
+        })
       }
     })
   }
@@ -35,15 +39,28 @@ export class FormComponent {
       cliente => {
         this.router.navigate(['/clientes'])
         swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con exito!`, 'success')
+      },
+      err => {
+        this.errores = err.error.errores as string[];
+        console.error('Código del error desde el backend' + err.status);
+        console.error(err.error.errores);
       }
-    )
+    );
   }
 
-  update(): void {
-    this.clienteService.update(this.cliente).subscribe( cliente => {
-      this.router.navigate(['/clientes'])
-      swal.fire('Cliente actualizado', `Cliente ${cliente.nombre} actualizado con éxito!`, 'success')
-    })
+  update():void{
+    this.clienteService.update(this.cliente).subscribe(
+      resp => {
+        console.log(this.cliente)
+        this.router.navigate(['/clientes']);
+        swal.fire('Cliente actualizado con exito', `El usuario "+ resp.nombre + " ha sido actualizado`, 'success');
+      },
+      err => {
+        this.errores = err.error.errores as string[];
+        console.error('Código del error desde el backend' + err.status);
+        console.error(err.error.errores);
+      }
+    )
   }
 
 }
