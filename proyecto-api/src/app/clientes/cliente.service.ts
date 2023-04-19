74ae28ecac009027610d26cecb3f7292da02eddf
ReynaldoA9 <reynaldo.silva.external@atos.net>
2023-04-19 11:02:50 +0100

    Terminamos carpeta 4

--- proyecto-api/src/app/clientes/cliente.service.ts
+++ proyecto-api/src/app/clientes/cliente.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-import { of,Observable } from 'rxjs';
-
+import { of,Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes';
 
-  getClientes(): Observable<Cliente[]> { 
-    return  of(CLIENTES); 
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<Cliente[]> {
+    //return  of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
   }
 }
